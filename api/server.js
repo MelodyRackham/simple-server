8210a01afba4882bec77ec4a033edee9e0d73feb
Mel Maki <melody.rackham@gmail.com>
2020-04-28 12:00:15 -0600

    Adds GET request

--- api/server.js
+++ api/server.js
@@ -7,6 +7,17 @@ const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use(expression.json());
+
+server.get('/', (req, res) => {
+  Shoutouts.find()
+    .then((shoutouts) => {
+      res.status(200).json(shoutouts);
+    })
+    .catch((error) => {
+      console.log('\nERROR', error);
+      res.status(500).json({ error: 'Cannot retrieve the shoutouts ' });
+    });
+});
 
 module.exports = server;
